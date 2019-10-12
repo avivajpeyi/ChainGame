36a777b0ee19f6bb776a33445bef3beeae644351
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2019-10-12 21:09:57 +1100

    add visuals

---
+++ Assets/Scripts/Transform/LookAlongVelocity.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+
+public class LookAlongVelocity : MonoBehaviour
+{
+	/*
+	 * 	LookAlongVelocity
+	 * 
+	 *	Author: Avi
+	 * 
+	 * 	MAKES THE SPRITE POINT TO THE VEL VECTOR
+	 */
+	
+	public float minVelocity = 0.01f;
+	public new Rigidbody2D rigidbody;
+
+	private void Update ()
+	{
+		if (rigidbody == null)
+			return;
+
+		if (rigidbody.velocity.magnitude < minVelocity)
+			return;
+
+		var rotation = transform.eulerAngles;
+
+		var angle = Vector2.SignedAngle (Vector2.up, rigidbody.velocity);
+		rotation.z = angle;
+
+		transform.eulerAngles = rotation;
+	}
+}
