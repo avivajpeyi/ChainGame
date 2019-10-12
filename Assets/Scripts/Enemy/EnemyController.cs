34dbb8981cf9ff4dbc717931f734dd0662512a2b
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2019-10-13 03:42:49 +1100

    add enemy shatter

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyController : MonoBehaviour
+{
+
+    public int enemyType = 0 ;
+    public SpriteRenderer renderer;
+    public GameObject effectsController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if (enemyType !=0)
+            renderer.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.transform.tag == "Player")
+        {
+            OnEnemyDeath(col.relativeVelocity);
+        }
+    }
+
+
+    void OnEnemyDeath(Vector2 relativeVelocity)
+    {
+        GameObject myEffects = Instantiate(effectsController, this.transform.position,  Quaternion.identity);
+        myEffects.GetComponent<EffectsController>().Play(relativeVelocity);
+        Destroy(this.gameObject);
+    }
+}
